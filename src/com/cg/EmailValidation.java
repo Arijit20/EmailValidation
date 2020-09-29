f8e41e81cdd1de28187e4468367aad31ca5b8d9e
Arijit20 <71620955+Arijit20@users.noreply.github.com>
2020-09-29 16:30:52 +0530

    Update EmailValidation.java

--- src/com/cg/EmailValidation.java
+++ src/com/cg/EmailValidation.java
@@ -10,7 +10,7 @@ public class EmailValidation {
 	public void emailValidator() {
 		System.out.println("Enter your Email :");
 		String email = in.next();
-		Pattern pattern = Pattern.compile("^([a-z]){1,}([.+_-]){0,1}[0-9a-z]*(@){1}([0-9a-z]*)(\\.([a-z]){2,}){1}(\\.[a-z][a-z])?$");
+		Pattern pattern = Pattern.compile("^([a-z]){1,}[a-z0-9]*([.+_-]){0,1}[0-9a-z]+(@){1}([0-9a-z]+)(\\.([a-z]){2,}){1}(\\.[a-z]{2,})?$");
 		Matcher matcher = pattern.matcher(email);
 		Boolean b = matcher.find();
 		if (b)
